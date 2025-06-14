d45d029f5e047fe6023d1140ca6219ad384bbd3b
Alan Alencar <alanalencar@me.com>
2025-06-14 19:51:31 -0300

    Initial Commit

---
+++ Stadiums/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Stadiums
+//
+//  Created by Alan Alencar on 14/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Stadiums/StadiumsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StadiumsApp.swift
+//  Stadiums
+//
+//  Created by Alan Alencar on 14/06/25.
+//
+
+import SwiftUI
+
+@main
+struct StadiumsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
